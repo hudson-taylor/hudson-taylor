cebba01a65defccde34b0b66dae9e9cad3c98909
Pomke Nohkan <pomke@pomke.com>
2014-07-28 11:37:58 +1000

    added repository line to package.json

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ ht = require('./index');
 var server = new ht.Server();
 server.add('echo', echoServiceSetup);
 server.listenHTTP({port: 7000});
-server.listenTCP({port: 7001});
 
 function echoServiceSetup(s, ready) {
     s.on("simple", {}, function(data, callback) {
@@ -16,16 +15,19 @@ function echoServiceSetup(s, ready) {
 
 // Connect to the service via HTTP and echo something
 var s = new ht.Services();
-s.connect("echoHTTP", new ht.HTTPClient("echo", "localhost", 7000));
-s.connect("echoTCP", new ht.TCPClient("echo", "localhost", 7001));
+s.connect("echo", new ht.HTTPClient("echo", "localhost", 7000));
 
-s.remote("echoTCP", "simple", { message : "hello world via TCP!" }, function(err, data) {
-    if(err) console.log(err);
-    console.log(data);
-});
-s.remote("echoHTTP", "simple", { message : "hello world via HTTP!" }, function(err, data) {
+s.remote("echo", "simple", { message : "hello world via HTTP!" }, function(err, data) {
     if(err) console.log(err);
     console.log(data);
 });
 
+
+//connect via TCP
+//server.listenTCP({port: 7001});
+//s.connect("echoTCP", new ht.TCPClient("echo", "localhost", 7001));
+//s.remote("echoTCP", "simple", { message : "hello world via TCP!" }, function(err, data) {
+//    if(err) console.log(err);
+//    console.log(data);
+//});
 
