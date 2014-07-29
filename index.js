58e5a7fb0c980a11c94a49a56c7f1e16d53564c8
Pomke <pomke@pomke.com>
2014-07-29 21:22:19 +1000

    Added LocalClient which allows local service hosting without RPC

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var schema = require('./lib/schema');
 
 // Public API
 exports.Server = server.Server;
+
 exports.Services = clients.Services;
 exports.HTTPClient = clients.HTTPClient;
 exports.TCPClient = clients.TCPClient;
+exports.LocalClient = clients.LocalClient;
+
 exports.validators = schema.validators;
 exports.makeParser = schema.makeParser;
