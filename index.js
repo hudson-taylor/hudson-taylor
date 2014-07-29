4526b4e9b714a9982429b432e4c721d3b72a06b7
Pomke <pomke@pomke.com>
2014-07-29 19:50:36 +1000

    Complete schema implementation, working tests

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var server = require('./lib/server');
 var clients = require('./lib/clients');
+var schema = require('./lib/schema');
 
 // Public API
 exports.Server = server.Server;
 exports.Services = clients.Services;
 exports.HTTPClient = clients.HTTPClient;
 exports.TCPClient = clients.TCPClient;
+exports.validators = schema.validators;
+exports.makeParser = schema.makeParser;
