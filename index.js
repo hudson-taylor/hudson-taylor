8aa0acb588500595fba5247dd52c97929dc38274
Adam Brady <adam@boxxen.org>
2014-10-02 20:19:09 +1000

    export utils

--- index.js
+++ index.js
@@ -3,3 +3,5 @@
 exports.Client     = require('./lib/client');
 exports.Service    = require('./lib/service');
 exports.Transports = require('./lib/transports');
+
+exports.utils = require('./lib/utils');
