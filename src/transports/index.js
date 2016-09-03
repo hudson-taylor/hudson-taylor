cc09bcc86fbb2bcecec655bd62a1aeb083e4d2a6
jasonk000 <jason.koch@iflix.com>
2016-09-04 00:12:44 +1000

    Added jsonrpc 2.0 over http transport

--- src/transports/index.js
+++ src/transports/index.js
@@ -4,3 +4,4 @@ exports.Local = require('./local')
 
 exports.HTTP = require('ht-http-transport')
 exports.TCP = require('ht-tcp-transport')
+exports.JSONRPCHTTP = require('ht-jsonrpc-http-transport')
